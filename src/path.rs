79cb4047b00261fc35c8e0ac57cee86bd57e9d09
Christophe Thibaut <cthibauttof@gmail.com>
2024-07-17 22:23:36 +0200

    check path for list of picture files

--- src/path.rs
+++ src/path.rs
@@ -1,4 +1,7 @@
 use walkdir::WalkDir;
+use std::fs;
+use std::path::PathBuf;
+
 use std::io::{Result, Error, ErrorKind};
 
 const VALID_EXTENSIONS: [&'static str; 6] = ["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"];
@@ -8,30 +11,72 @@ pub const THUMB_SUFFIX: &str = "THUMB";
 pub const IMAGE_DATA: &str = "IMAGE_DATA";
 
 
-pub fn get_picture_file_paths(directory: &str) -> Result<Vec<String>> {
-    let mut file_paths: Vec<String> = Vec::new();
-    for path in WalkDir::new(directory).into_iter().filter_map(|e| e.ok())
-        .map(|e| e.into_path()) {
-            let valid_extension = match path.extension() {
-                Some(extension) => VALID_EXTENSIONS.contains(&extension.to_str().unwrap()),
-                None => false,
-            };
-            let not_a_thumbnail = match path.to_str().map(|f| f.contains(THUMB_SUFFIX)) {
-                Some(false) => true,
-                _ => false,
-            };
-            if path.is_file() && valid_extension && not_a_thumbnail {
-                file_paths.push((&path.display()).to_string())
-            }
-        };
-    Ok(file_paths.clone())
+pub fn check_path(source: &str) -> Result<PathBuf> {
+    let path = PathBuf::from(source);
+    if !path.exists() {
+        Err(Error::new(ErrorKind::Other, format!("path {} doesn't exist", source)))
+    } else {
+        match fs::metadata(path.clone()) {
+            Ok(metadata) => if metadata.is_dir() {
+                Ok(path)
+            } else {
+                Err(Error::new(ErrorKind::Other, format!("path {} is not a directory", source)))
+            },
+            Err(err) => Err(err),
+        }
+    }
+}
+
+pub fn get_picture_file_paths(source: &str) -> Result<Vec<String>> {
+    match check_path(source) {
+        Ok(directory) => {
+            let mut file_paths: Vec<String> = Vec::new();
+            for path in WalkDir::new(directory).into_iter().filter_map(|e| e.ok())
+                .map(|e| e.into_path()) {
+                    let valid_extension = match path.extension() {
+                        Some(extension) => VALID_EXTENSIONS.contains(&extension.to_str().unwrap()),
+                        None => false,
+                    };
+                    let not_a_thumbnail = match path.to_str().map(|f| f.contains(THUMB_SUFFIX)) {
+                        Some(false) => true,
+                        _ => false,
+                    };
+                    if path.is_file() && valid_extension && not_a_thumbnail {
+                        file_paths.push((&path.display()).to_string())
+                    }
+                };
+            Ok(file_paths.clone())
+        },
+        Err(err) => Err(err),
+    }
 }
 
 
-#[test]
-fn get_all_pictures_including_sub_folders_except_thumbnails() {
-    let result = get_picture_file_paths("testdata");
-    assert_eq!(true, result.is_ok());
-    let file_paths = result.unwrap();
-    assert_eq!(10, file_paths.len());
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn get_all_pictures_including_sub_folders_except_thumbnails() {
+        let result = get_picture_file_paths("testdata");
+        assert_eq!(true, result.is_ok());
+        let file_paths = result.unwrap();
+        assert_eq!(10, file_paths.len());
+    }
+
+    #[test]
+    fn get_an_error_on_absent_directory() {
+        let result = get_picture_file_paths("foo");
+        assert_eq!(false, result.is_ok());
+        assert_eq!("path foo doesn't exist", result.unwrap_err().to_string());
+    }
+
+    #[test]
+    fn get_an_error_on_not_a_directory() {
+        let result = get_picture_file_paths("testdata/nature/flower.jpg");
+        assert_eq!(false, result.is_ok());
+        assert_eq!("path testdata/nature/flower.jpg is not a directory", result.unwrap_err().to_string());
+    }
+
 }
