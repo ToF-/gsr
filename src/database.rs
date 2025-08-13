1a4da3823766fa443a36a2607a0c4a9114c684dd
Christophe Thibaut <cthibauttof@gmail.com>
2025-08-14 01:45:08 +0200

    sqlite not working

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use sqlite::State;
 use anyhow::{anyhow, Result};
 use sqlite::Connection;
@@ -17,37 +18,52 @@ impl Database {
 
     pub fn initialize() -> Result<Self> {
         if let Ok(connection_string) = &env::var(DATABASE_CONNECTION) {
+            let path = Path::new(connection_string);
+            if path.exists() {
+                match sqlite::open(connection_string) {
+                    Ok(connection) => {
+                        println!("opening {}", connection_string);
+                        return Ok(Database {
+                            connection_string: connection_string.to_string(),
+                            connection: connection,
+                        })
+                    },
+                    Err(err) => return Err(anyhow!(err)),
+                }
+            }
             match sqlite::open(connection_string) {
                 Ok(connection) => {
-                    println!("opening {}", connection_string);
-                    Ok(Database {
-                        connection_string: connection_string.to_string(),
-                        connection: connection,
-                    })
+                    let query = "CREATE TABLE Picture ( file_path TEXT PRIMARY KEY );";
+                    match connection.execute(query) {
+                        Ok(_) => Ok(Database {
+                            connection_string: connection_string.to_string(),
+                            connection: connection,
+                        }),
+                        Err(err) => Err(anyhow!(err)),
+                    }
                 },
                 Err(err) => Err(anyhow!(err)),
-                }
+            }
         } else {
             Err(anyhow!("the database connection string can't be read. Did you define GALLSHDB?"))
         }
-
     }
 
     pub fn check_create_schema(&self, catalog: &Catalog) -> Result<()> {
         println!("checking database {} picture table", self.connection_string);
-        let query = "SELECT Count(*) from Picture";
+        let query = "SELECT file_path from Picture";
         let mut count: i64 = 0;
-        let query = "SELECT count(*) FROM Picture";
         match self.connection.prepare(query) {
             Ok(mut statement) => {
                 while let Ok(State::Row) = statement.next() {
-                    count = statement.read::<i64, _>("count(*)").unwrap();
+                    count +=1;
                 }
             },
             Err(err) => {
                 println!("{err}");
             }
         };
+        println!("{} records found in picture table", count);
         if count == 0 {
             println!("the database {} does not contain a Picture table. Create the database tables?", self.connection_string);
             let mut response = String::new();
@@ -55,28 +71,13 @@ impl Database {
             stdin.read_line(&mut response).expect("can't read from stdin");
             match response.chars().next() {
                 Some(ch) if ch == 'y' || ch == 'Y' => {
-                    println!("creating tables in database {}", self.connection_string);
-                    match self.create_tables() {
-                        Ok(()) => {
-                            return self.populate_tables(catalog)
-                        },
-                        err => err,
-                    }
-
+                    return self.populate_tables(catalog)
                 },
                 _ => Err(anyhow!("database does not contain Picture table")),
             }
 
         } else {
             Ok(())
-        }
-    }
-
-    fn create_tables(&self) -> Result<()> {
-        let query = "CREATE TABLE Picture ( file_path TEXT PRIMARY KEY );";
-        match self.connection.execute(query) {
-            Ok(_) => Ok(()),
-            Err(err) => Err(anyhow!(err)),
         }
     }
 
