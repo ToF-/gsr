45d5d0de2dcd33a381f4480319b35960d0a25183
Christophe Thibaut <cthibauttof@gmail.com>
2025-08-14 15:27:26 +0200

    palette to blob and vice versa

--- src/palette.rs
+++ src/palette.rs
@@ -18,15 +18,27 @@ pub fn get_palette(image: &DynamicImage) -> Palette {
 
 pub fn palette_to_blob(palette: &Palette) -> [u8;36] {
     let mut result: [u8;36] = [0; 36];
-    for i in 0..8 {
+    let mut pos: usize = 0;
+    for i in 0..9 {
         let mut value: u32 = palette[i];
-        for j in 0..3 {
-            let pos:usize = i * 4 + j;
+        for j in 0..4 {
             result[pos] = (value & 255) as u8;
+            pos += 1;
             value = value >> 8;
         }
     }
     return result;
+}
+
+pub fn blob_to_palette(blob: &[u8;36]) -> Palette {
+    let mut result: Palette = [0;9];
+    for i in 0..9 {
+        for j in 0..4 {
+            let pos:usize = i * 4 + j;
+            result[i] = result[i] | (blob[pos] as u32) << j*8;
+        }
+    }
+    result
 }
 
 fn rgba_key(rgba: Rgba<u8>) -> u32 {
@@ -46,4 +58,21 @@ pub fn get_colors(image: &DynamicImage) -> usize {
     };
     colors.len()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convert_to_blob_and_vice_versa() {
+        let expected: Palette = [0x23174807u32, 0x11223344u32, 0x44332211u32, 0x48072317u32, 0xdeadbeefu32, 0x0a0b0c0du32,0x00000000u32,0x12345678u32,0xfedcba98u32];
+        let blob: [u8;36] = palette_to_blob(&expected);
+        let result: Palette = blob_to_palette(&blob);
+        for i in 0..8 {
+            assert_eq!(result[i],expected[i]);
+        }
+
+    }
+}
+
 
